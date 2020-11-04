b3c163cd8c8ea2520db6e06706cb9322de182229
shimin mathew <simplyshimin@gmail.com>
2020-11-04 23:45:14 +0530

    add app-header

---
+++ src/app/header/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './header.css';
+
+export default function Header() {
+	return (
+		<header>
+			<h1 className="logo">
+				<span className="red-bg">COVID</span>
+				<span className="wh-txt">india</span>
+			</h1>
+		</header>
+	)
+}
