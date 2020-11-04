eede695e3e599756c13e17833bf573eb73656a4a
shimin mathew <simplyshimin@gmail.com>
2020-11-04 23:11:32 +0530

    add routes to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import HomePage from 'pages/home';
+import StateViewPage from 'pages/state-view';
 
 function App() {
   return (
-    <main></main>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/states/:stateId" component={StateViewPage} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
