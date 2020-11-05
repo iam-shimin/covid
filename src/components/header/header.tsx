709212d375b320993c2c5d1d5a0fbbafb2d585a0
shimin mathew <simplyshimin@gmail.com>
2020-11-05 17:55:53 +0530

    add header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './header.css';
+
+
+function Header() {
+	return (
+		<header>
+			<h1 className="logo">
+				<span className="red-bg">COVID</span>
+				<span className="wh-txt">india</span>
+			</h1>
+		</header>
+	)
+}
+
+export default Header;
