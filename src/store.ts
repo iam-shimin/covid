e36c91b83eb57687fbd21e0ccfa70acc81f791e4
shimin mathew <simplyshimin@gmail.com>
2020-11-05 17:57:04 +0530

    add thunk to store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,10 @@
-import { combineReducers, createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import statsReducer from 'reducers/stats';
 
-const rootReducer = combineReducers({
-	stats: statsReducer
-});
-
-export default createStore(rootReducer, composeWithDevTools());
+export default createStore(
+	statsReducer,
+	composeWithDevTools(applyMiddleware(thunkMiddleware))
+);
