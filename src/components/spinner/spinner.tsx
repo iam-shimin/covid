c17bebaaf0d1d3e2e531d7f71c7ceac193fafb6a
shimin mathew <simplyshimin@gmail.com>
2020-11-05 19:15:50 +0530

    add spinner

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './spinner.css';
+
+interface StateProps {
+	isLoading: boolean
+}
+
+function Spinner({isLoading}: StateProps) {
+	return isLoading
+		?	<div className="spinner" />
+		:	null;
+}
+
+const mapStateToProps = ({isLoading}: IStatsReduxState) => ({
+	isLoading
+})
+
+export default connect(mapStateToProps)(Spinner);
