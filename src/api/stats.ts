ea4206b389d1cf8caaca48bf8a7486e83f466d9e
shimin mathew <simplyshimin@gmail.com>
2020-11-05 17:53:17 +0530

    api call 'getStatsForStates' should return only statewise data

--- src/api/stats.ts
+++ src/api/stats.ts
@@ -2,5 +2,5 @@ import get from './__http__';
 
 export default function getStatsForStates() {
 	return get('/data.json')
-		.then(response => response.data)
+		.then(response => response.data.statewise)
 }
