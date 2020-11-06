83a549fb2890edefd1bbb3d7a3d2b754e7819e07
shimin mathew <simplyshimin@gmail.com>
2020-11-06 16:38:12 +0530

    add indian states latlng to constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,153 +2,159 @@ export const STATS_LOADING = 'STATS_LOADING';
 export const STATS_ERROR = 'STATS_ERROR';
 export const STATS_SUCCESS = 'STATS_SUCCESS';
 
+export const STAT_DISTRICT_LOADING = 'STAT_DISTRICT_LOADING';
+export const STAT_DISTRICT_ERROR = 'STAT_DISTRICT_ERROR';
+export const STAT_DISTRICT_SUCCESS = 'STAT_DISTRICT_SUCCESS';
+
+type LatLng = [number, number];
+
 export const INDIAN_STATE_LATLNG = {
 	"MH": [
 		18.9068356,
 		75.6741579
-	],
+	] as LatLng,
 	"AP": [
 		15.9240905,
 		80.1863809
-	],
+	] as LatLng,
 	"KA": [
 		14.5203896,
 		75.7223521
-	],
+	] as LatLng,
 	"TN": [
 		10.9094334,
 		78.3665347
-	],
+	] as LatLng,
 	"UP": [
 		27.1303344,
 		80.859666
-	],
+	] as LatLng,
 	"KL": [
 		10.3528744,
 		76.5120396
-	],
+	] as LatLng,
 	"DL": [
 		28.6517178,
 		77.2219388
-	],
+	] as LatLng,
 	"WB": [
 		22.9964948,
 		87.6855882
-	],
+	] as LatLng,
 	"OR": [
 		20.5431241,
 		84.6897321
-	],
+	] as LatLng,
 	"TG": [
 		17.8495919,
 		79.1151663
-	],
+	] as LatLng,
 	"BR": [
 		25.6440845,
 		85.906508
-	],
+	] as LatLng,
 	"AS": [
 		26.4073841,
 		93.2551303
-	],
+	] as LatLng,
 	"RJ": [
 		26.8105777,
 		73.7684549
-	],
+	] as LatLng,
 	"CT": [
 		21.6637359,
 		81.8406351
-	],
+	] as LatLng,
 	"GJ": [
 		22.3850008,
 		71.7452633
-	],
+	] as LatLng,
 	"MP": [
 		23.8143419,
 		77.5340719
-	],
+	] as LatLng,
 	"HR": [
 		29,
 		76
-	],
+	] as LatLng,
 	"PB": [
 		30.9293211,
 		75.5004841
-	],
+	] as LatLng,
 	"JH": [
 		23.4559809,
 		85.2557301
-	],
+	] as LatLng,
 	"JK": [
 		33.5574473,
 		75.06152
-	],
+	] as LatLng,
 	"UT": [
 		30.0919935,
 		79.3217666
-	],
+	] as LatLng,
 	"GA": [
 		15.3004543,
 		74.0855134
-	],
+	] as LatLng,
 	"PY": [
 		11.9340568,
 		79.8306447
-	],
+	] as LatLng,
 	"TR": [
 		23.7750823,
 		91.7025091
-	],
+	] as LatLng,
 	"HP": [
 		31.8167602,
 		77.3493205
-	],
+	] as LatLng,
 	"MN": [
 		24.7208818,
 		93.9229386
-	],
+	] as LatLng,
 	"AR": [
 		28.0937702,
 		94.5921326
-	],
+	] as LatLng,
 	"CH": [
 		30.7334421,
 		76.7797143
-	],
+	] as LatLng,
 	"ML": [
 		25.5379432,
 		91.2999102
-	],
+	] as LatLng,
 	"NL": [
 		26.1630556,
 		94.5884911
-	],
+	] as LatLng,
 	"LA": [
 		33.9456407,
 		77.6568576
-	],
+	] as LatLng,
 	"AN": [
 		12.6112387,
 		92.8316541
-	],
+	] as LatLng,
 	"SK": [
 		27.601029,
 		88.4541364
-	],
+	] as LatLng,
 	"DN": [
 		20.7179857,
 		70.9323992
-	],
+	] as LatLng,
 	"MZ": [
 		23.2146169,
 		92.8687612
-	],
+	] as LatLng,
 	"UN": [
 		-17.2654258,
 		119.3571335
-	],
+	] as LatLng,
 	"LD": [
 		10.8130838,
 		73.6805344
-	]
-}
+	] as LatLng
+};
