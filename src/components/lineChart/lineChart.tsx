44a67726d252b9bcf0ba2a523fea07d3eee2945a
shimin mathew <simplyshimin@gmail.com>
2020-11-06 16:34:21 +0530

    add linechart

---
+++ src/components/lineChart/lineChart.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+import { Data } from 'plotly.js';
+
+interface Line {
+	name: string,
+	data: number[]
+}
+
+export default function LineChart() {
+
+	// let data = lines.map(line => ({
+	// 	name: line.name,
+	// 	type: 'scatter',
+	// 	x: line.data.map((_: any, i: number) => i),
+	// 	y: line.data
+	// }));
+	
+	return (
+		<Plot
+			data={['active', 'confirmed', 'deaths', 'recovered'].map(i => ({
+				name: i,
+				type: 'scatter',
+				...genXY()
+			}))}
+
+			layout={{
+				height: 400,
+				width: 500
+		}} />
+	)
+}
+
+
+function genXY() {
+	return {
+		x: Array.from({length: 5}, (_: any, i: number) => i),
+		y: Array.from({length: 5}, () => Math.floor(Math.random() * 1000))
+	}
+}
