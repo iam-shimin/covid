5505bb8bcb5e90a3db675f1f41cae3d0cfe81060
shimin mathew <simplyshimin@gmail.com>
2020-11-12 12:46:59 +0530

    use API data for line chart instead of random data

--- src/components/lineChart/lineChart.tsx
+++ src/components/lineChart/lineChart.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-export default function LineChart() {
+export default function LineChart({ data }: { data: any }) {
+
+	if (!Array.isArray(data)) {
+		return null;
+	}
+	
+	const fields: { [_: string]: string} = {
+		'active': 'totalconfirmed',
+		'confirmed': 'dailyconfirmed',
+		'deaths': 'totaldeceased',
+		'recovered': 'dailyrecovered'
+	};
+
 	return (
 		<Plot
 			data={['active', 'confirmed', 'deaths', 'recovered'].map(i => ({
 				name: i,
 				type: 'scatter',
-				...genXY()
+				...genXY(fields[i], data)
 			}))}
 
 			layout={{
@@ -18,9 +30,13 @@ export default function LineChart() {
 }
 
 
-function genXY() {
+function genXY(key: string, data: any[]) {
 	return {
-		x: Array.from({length: 5}, (_: any, i: number) => i),
-		y: Array.from({length: 5}, () => Math.floor(Math.random() * 1000))
-	}
+		x: data.map(i => i.dateymd),
+		y: data.map(i => parseInt(i[key]))
+	};
+	// {
+	// 	x: Array.from({length: 5}, () => data),
+	// 	y: Array.from({length: 5}, () => Math.floor(Math.random() * 1000))
+	// }
 }
